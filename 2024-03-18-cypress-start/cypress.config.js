997e6406cc004899cdf5df42a98b9548505e7985
Lina <lina.puksme@gmail.com>
2024-03-20 17:49:31 +0200

    cypress first tests

--- 2024-03-18-cypress-start/cypress.config.js
+++ 2024-03-18-cypress-start/cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    username: 'lina.puksme@gmail.com',
+    password: 'lina.puksme@gmail.com',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
